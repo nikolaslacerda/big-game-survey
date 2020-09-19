438b569e5d378409d0f8062453b3b8a9bf9c1721
nikolaslacerda <nikolas.lacerda@hotmail.com>
2020-09-19 20:50:22 -0300

    add get records

--- server/src/main/java/com/dssurvey/server/controllers/RecordController.java
+++ server/src/main/java/com/dssurvey/server/controllers/RecordController.java
@@ -1,11 +1,18 @@
 package com.dssurvey.server.controllers;
 
+import java.time.Instant;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dssurvey.server.dto.RecordDTO;
@@ -23,6 +30,32 @@ public class RecordController {
 	public ResponseEntity<RecordDTO> insert(@RequestBody RecordInsertDTO recordInsertDTO) {
 		RecordDTO newDTO = recordService.insert(recordInsertDTO);
 		return ResponseEntity.status(HttpStatus.OK).body(newDTO);
+	}
+	
+	@GetMapping
+	public ResponseEntity<Page<RecordDTO>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
+												   @RequestParam(value = "linesPerPage", defaultValue = "0") Integer linesPerPage,
+												   @RequestParam(value = "orderBy", defaultValue = "moment") String orderBy,
+												   @RequestParam(value = "direction", defaultValue = "DESC") String direction,
+												   @RequestParam(value = "min", defaultValue = "") String min,
+												   @RequestParam(value = "max", defaultValue = "") String max
+												   ) {
+		
+		Instant minData = ("".equals(min)) ? null :Instant.parse(min);
+		Instant maxData = ("".equals(max)) ? null :Instant.parse(max);
+		
+		if ( linesPerPage == 0 ) {
+			linesPerPage = Integer.MAX_VALUE;
+		}
+		
+		System.out.println(minData + "   " + maxData);
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		Page<RecordDTO> list = recordService.findByMoments(minData, maxData, pageRequest);
+		return ResponseEntity.status(HttpStatus.OK).body(list);
 		
 	}
+	
+	
 }
